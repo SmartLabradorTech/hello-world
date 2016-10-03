4f9e34e9749631eb14fef905c4f5fc10600e50d1
SmartLabradorTech <smarthuskytech@hotmail.com>
2016-10-02 22:36:16 -0700

    Add a new class

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tianleh on 10/2/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int a = 1;
+
+        System.out.println("a is " + a);
+    }
+}
